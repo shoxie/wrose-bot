d6db66fc4274dc04308acbe2a80efa05455c1a83
shoxie <sktt1lka@gmail.com>
2019-12-25 14:08:49 +0700

    play

--- commands/music/play.js
+++ commands/music/play.js
@@ -5,69 +5,42 @@ module.exports = {
   name: "play",
   catetory: "music",
   run: async (message, args) => {
-    const playlist = fs.existsSync("./data/playlist.json");
-    if (!playlist) {
-      fs.writeFile("./data/playlist.json", "{}", err => {
-        if (err) console.log(err);
-      });
-    }
-    var voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel)
-      return message.channel.send({
-        embed: {
-          description: "Vào voice channel đã."
-        }
-      });
+    let voiceChannel = message.member.voiceChannel;
+    if (!voiceChannel) return message.channel.send("vao voice da~");
     var musicModel = {
       isPlaying: false,
       queue: [],
       connection: null
     };
-    let songInfo = null;
-    if (ytdl.validateURL(args[0])) {
-      songInfo = await ytdl.getInfo(args[0]);
-      musicModel.queue.push(songInfo.url);
-      musicModel.connection = await voiceChannel.join();
-      play(message, songInfo, song);
-    }
+    queue();
+    async function queue() {
+      let songInfo = null;
 
-    var song = {
-      title: songInfo.title,
-      url: songInfo.video_url,
-      thumbnailUrl: songInfo.player_response.videoDetails.thumbnail.thumbnails.pop()
-        .url,
-      duration: new Date(songInfo.length_seconds * 1000)
-        .toISOString()
-        .substr(11, 8)
-    };
-
-    function play(message, songInfo, song) {
-      var queue = musicModel.queue;
-      console.log(queue[0]);
-      if (!queue[0]) {
-        musicModel.isPlaying = false;
+      if (ytdl.validateURL(args[0])) {
+        if ((musicModel.isPlaying = true))
+          return musicModel.queue.push(args[0]);
+        else {
+          musicModel.queue.push(args[0]);
+          musicModel.connection = await voiceChannel.join();
+          play();
+        }
       }
-      musicModel.isPlaying = true;
-
+    }
+    function play() {
+      var queue = musicModel.queue;
       const dispatcher = musicModel.connection
         .playStream(
-          ytdl(songInfo.video_url, {
+          ytdl(queue[0], {
             filter: "audioonly",
             quality: "highestaudio",
             highWaterMark: 1 << 25
           })
         )
         .on("start", () => {
-          console.log("Music started!");
-          message.channel.send("playing");
+          console.log("playing");
         })
         .on("end", () => {
-          console.log("Music ended!");
-          queue.shift();
-          play(message, song);
-        })
-        .on("error", error => {
-          console.error(error);
+          queue();
         });
     }
   }
