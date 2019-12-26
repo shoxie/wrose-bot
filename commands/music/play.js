14d309c3a4e8ae8fbf2f99f8fa5e2296c94c6df5
shoxie <sktt1lka@gmail.com>
2019-12-26 13:32:16 +0700

    daily commit

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,33 +1,44 @@
 const fs = require("fs");
 const ytdl = require("ytdl-core");
-const { YouTube } = require("better-youtube-api");
+const {
+  YouTube
+} = require("better-youtube-api");
+// test xem
+const musicModel = require('../../model/model.js');
+
+/*
+ qua folder test
+*/
+
+
 module.exports = {
   name: "play",
   catetory: "music",
   run: async (message, args) => {
     let voiceChannel = message.member.voiceChannel;
     if (!voiceChannel) return message.channel.send("vao voice da~");
-    var musicModel = {
-      isPlaying: false,
-      queue: [],
-      connection: null
-    };
-    queue();
-    async function queue() {
-      let songInfo = null;
-      if (ytdl.validateURL(args[0])) {
-        musicModel.queue.push(args[0]);
-        musicModel.connection = await voiceChannel.join();
-        play();
-      }
-    }
+
+    musicModel.queue(args, voiceChannel);
+    // async function queue() {
+    //   let songInfo = null;
+    //   if (ytdl.validateURL(args[0])) {
+    //     if (musicModel.isPlaying == true) {
+    //       return musicModel.queue.push(args[0]);
+    //     }
+
+    //     musicModel.queue.push(args[0]);
+    //     musicModel.connection = await voiceChannel.join();
+    //     play();
+    //   }
+    // }
+
     function play() {
       var queue = musicModel.queue;
-      if ((musicModel.isPlaying = true)) return musicModel.queue.push(args[0]);
       if (!queue[0]) {
         voiceChannel.leave();
         musicModel.isPlaying = false;
       }
+      console.log(queue);
       musicModel.isPlaying = true;
       const dispatcher = musicModel.connection
         .playStream(
@@ -41,6 +52,8 @@ module.exports = {
           console.log("playing");
         })
         .on("end", () => {
+          musicModel.isPlaying = false;
+          queue.shift();
           play();
         });
     }
---
+++ model/model.js
@@ -0,0 +1,46 @@
+const ytdl = require("ytdl-core");
+let message;
+let musicModel = {
+  isPlaying: false,
+  queue: [],
+  connectinon: null,
+  songInfo: this.songInfo,
+  init() {
+    msg = message;
+    isInit = true;
+  },
+  async queue(args, voiceChannel) {
+    if (ytdl.validateURL(args[0])) {
+      if (this.isPlaying == true) {
+        this.queue.push(args[0]);
+      }
+      musicModel.queue.pus(args[0]);
+      musicModel.connection = await voiceChannel.join();
+      play();
+    }
+  },
+  play(args) {
+    const dispatcher = this.musicModel.connection.playStream(
+      ytdl(this.queue[0], {
+        filter: "audioonly",
+        quality: "highestaudio",
+        highWaterMark: 1 << 25
+      })
+        .on("start", () => {
+          console.log("playing");
+          this.isPlaying = true;
+        })
+        .on("end", () => {
+          this.isPlaying = false;
+          this.play();
+        })
+    );
+  },
+  async getSongInfo() {
+    if (ytdl.validateURL(args[0])) {
+      songInfo = await ytdl.getInfo(args[0]);
+    }
+    return songInfo;
+  }
+};
+module.exports = musicModel;
