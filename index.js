f25b6536f41fecb6c188cb553f6e5a8d5934c766
shoxie <sktt1lka@gmail.com>
2020-02-14 14:27:18 +0700

    idk

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config/config.json");
 process.env.NODE_PATH = __dirname;
-
+require('dotenv').config()
 const eventHandler = require("./handler/eventHandler.js")(client);
 
-client.login(config.token);
+client.login(process.env.token);
