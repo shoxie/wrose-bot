6283495567004c56080a7aa35e23c0679f81468f
shoxie <sktt1lka@gmail.com>
2020-02-23 17:54:07 +0700

    Co-authored-by: Ngô Phú Thịnh

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,11 +1,11 @@
 module.exports = {
-    config: {
-        name: 'ping',
-        usage: 'playlist --arguement [playlist name] [url]',
-        description: 'Show top songs played by me',
-        enabled: true
-    },
-    run: async (message, args) => {
-        message.channel.send('pong')
-    }
-}
+  config: {
+    name: "ping",
+    usage: "playlist --arguement [playlist name] [url]",
+    description: "Show top songs played by me",
+    enabled: false
+  },
+  run: async (message, args) => {
+    message.channel.send("pong");
+  }
+};
