7ef9b8484a8be35fa8bcc86e194b34f5faa9c188
shoxie <sktt1lka@gmail.com>
2020-03-16 15:57:45 +0700

    shit happens

--- commands/music/nowPlaying.js
+++ commands/music/nowPlaying.js
@@ -9,47 +9,48 @@ module.exports = {
   },
   async run(client, message, args) {
     const serverQueue = client.queue.get(message.guild.id);
-    if (!serverQueue.queue[0]) {
+    if (!serverQueue) {
       return message.channel.send({
         embed: {
           color: 15158332,
           title: "I'm not playing anything right now"
         }
       });
-    }
-    message.channel.send({
-      embed: {
-        color: 3447003,
-        title: "Now playing",
-        url: serverQueue.queue[0].url,
-        fields: [
-          {
-            name: "Song name",
-            value: serverQueue.queue[0].title
+    } else {
+      message.channel.send({
+        embed: {
+          color: 3447003,
+          title: "Now playing",
+          url: serverQueue.queue[0].url,
+          fields: [
+            {
+              name: "Song name",
+              value: serverQueue.queue[0].title
+            },
+            {
+              name: "Duration",
+              value: serverQueue.queue[0].duration
+            },
+            {
+              name: "Requested by",
+              value: serverQueue.queue[0].requester
+            }
+          ],
+          thumbnail: {
+            url: message.client.user.avatarURL({
+              format: "png",
+              dynamic: true,
+              size: 1024
+            })
           },
-          {
-            name: "Duration",
-            value: serverQueue.queue[0].duration
+          image: {
+            url: serverQueue.queue[0].thumbnail
           },
-          {
-            name: "Requested by",
-            value: serverQueue.queue[0].requester
+          footer: {
+            text: "Created by wrose"
           }
-        ],
-        thumbnail: {
-          url: message.client.user.avatarURL({
-            format: "png",
-            dynamic: true,
-            size: 1024
-          })
-        },
-        image: {
-          url: serverQueue.queue[0].thumbnail
-        },
-        footer: {
-          text: "Created by wrose"
         }
-      }
-    });
+      });
+    }
   }
 };
