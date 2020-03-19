52c805a5b4d4df31a64a89bedcd23a7417b84cc2
wrose <sktt1lka@gmail.com>
2020-03-20 01:23:14 +0700

    Update skip.js

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -26,7 +26,7 @@ module.exports = {
       });
     }
     if (message.member.guild.id === "335604901730058243") {
-      if (!message.member.roles.cache.has("520470163653525505")) {
+      if (!message.member.hasPermissions('ADMINISTRATOR') {
         return;
       }
     } else if (message.member.voice.channel != serverQueue.voiceChannel) {
