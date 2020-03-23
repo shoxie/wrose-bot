8356e8b21c867ebb93486e329f70ff06c82cffae
shoxie <sktt1lka@gmail.com>
2020-03-23 23:40:14 +0700

    dit

--- commands/music/disconnect.js
+++ commands/music/disconnect.js
@@ -9,6 +9,18 @@ module.exports = {
   },
   async run(client, message, args) {
     let serverQueue = client.queue.get(message.guild.id);
+    let requiredRole = message.guild.roles.cache.find(
+      x => x.name === "DJ" || "dj"
+    );
+    if (!requiredRole) {
+      return message.channel.send({
+        embed: {
+          color: 15158332,
+          title:
+            "Your guild does not have the DJ role, please contact your guild owner/administrator/moderator to create and add the role."
+        }
+      });
+    }
     if (!serverQueue.voiceChannel) {
       return message.channel.send({
         embed: {
@@ -27,20 +39,24 @@ module.exports = {
       });
     }
     if (serverQueue.voiceChannel) {
-      serverQueue.connection.dispatcher.end();
-      message.channel.send({
-        embed: {
-          title: "Disconnected from voiceChannel",
-          author: {
-            name: message.client.user.username,
-            icon_url: message.client.user.avatarURL({
-              format: "png",
-              dynamic: true,
-              size: 1024
-            })
+      if (
+        message.member.roles.cache.has(
+          message.guild.roles.cache.find(x => x.name === "DJ" || "dj")).id) {
+        serverQueue.connection.dispatcher.end();
+        message.channel.send({
+          embed: {
+            title: "Disconnected from voiceChannel",
+            author: {
+              name: message.client.user.username,
+              icon_url: message.client.user.avatarURL({
+                format: "png",
+                dynamic: true,
+                size: 1024
+              })
+            }
           }
-        }
-      });
+        });
+      } else return message.channel.send("Mày định đảo chính hả ???");
     }
   }
 };
