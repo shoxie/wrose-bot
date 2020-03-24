dff15a1d91b0937e133e81c23e4d7eb36c53fb4f
shoxie <sktt1lka@gmail.com>
2020-03-24 11:14:03 +0700

    test

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -8,63 +8,11 @@ module.exports = {
     enabled: true
   },
   async run(client, message, args) {
-    // let serverQueue = client.queue.get(message.guild.id);
-    // let requiredRole = await message.guild.roles.cache.find(
-    //   x => x.name === "DJ" || "dj"
-    // );
-    // if (!message.member.roles.cache.has(message.guild.roles.cache.find(x => x.name === "DJ" || "dj").id))
-    // if (!requiredRole) {
-    //   return message.channel.send({
-    //     embed: {
-    //       color: 15158332,
-    //       title:
-    //         "Your guild does not have the DJ role, please contact your guild owner/administrator/moderator to create and add the role."
-    //     }
-    //   });
-    // }
-    // if (!serverQueue) {
-    //   return message.channel.send({
-    //     embed: {
-    //       color: 15158332,
-    //       title: "No songs in the queue",
-    //       author: {
-    //         name: message.client.user.username,
-    //         icon_url: message.client.user.avatarURL({
-    //           format: "png",
-    //           dynamic: true,
-    //           size: 1024
-    //         })
-    //       }
-    //     }
-    //   });
-    // }
-
-    // if (!message.member.roles.cache.has(message.guild.roles.cache.find(x => x.name === "DJ" || "dj").id)) {
-    //   return message.channel.send({
-    //     embeD: {
-    //       color: 15158332,
-    //       title: "You do not have the DJ role."
-    //     }
-    //   });
-    // } else if (message.member.voice.channel != serverQueue.voiceChannel) {
-    //   // undefined
-    //   return message.channel.send({
-    //     embed: {
-    //       title:
-    //         "You have to be in the same channel with the me to use the command",
-    //       author: {
-    //         name: message.client.user.username,
-    //         icon_url: message.client.user.avatarURL({
-    //           format: "png",
-    //           dynamic: true,
-    //           size: 1024
-    //         })
-    //       }
-    //     }
-    //   });
-    // } else serverQueue.connection.dispatcher.end();
     let serverQueue = client.queue.get(message.guild.id);
-    if (!message.guild.roles.cache.has(x => x.name === "dj" || "DJ")) {
+    let normaldj = message.guild.roles.cache.find(x => x.name === 'dj');
+    let bigdj = message.guild.roles.cache.find(x => x.name === 'DJ');
+    let roleID = bigdj ? bigdj : normaldj;
+    if (!roleID) {
       return message.channel.send({
         embed: {
           color: 15158332,
@@ -73,7 +21,7 @@ module.exports = {
         }
       });
     }
-    if(message.member.roles.cache.has(x => x.name === 'dj' || 'DJ')) {
+    if(message.member.roles.cache.has(roleID.id)) {
       if (message.member.voice.channel != serverQueue.voiceChannel) {
           // undefined
           return message.channel.send({
