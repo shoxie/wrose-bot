305170d155931c90f6429fb5341558bad3da838b
shoxie <sktt1lka@gmail.com>
2020-03-25 20:50:24 +0700

    lul

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,5 +1,15 @@
 module.exports = client => {
-  return async function(oldMember, newMember) {
-      const serverQueue = client.queue.get(newMember.guild.id)
+  return async function(oldState, newState) {
+    const serverQueue = client.queue.get(newState.guild.id);
+    if (!serverQueue) return;
+    if (serverQueue) {
+      if (serverQueue.voiceChannel.members.size < 2) {
+        setTimeout(() => {
+          serverQueue.queue = [];
+          serverQueue.textChannel.send("Tao đã chờ ở đây quá lâu rồi!");
+          serverQueue.connection.dispatcher.end();
+        }, 10000);
+      }
+    }
   };
 };
