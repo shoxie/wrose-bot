659fa05a741c05784e1674e9cbe52e944c337be3
shoxie <sktt1lka@gmail.com>
2020-04-07 09:06:40 +0700

    handle

--- commands/music/play.js
+++ commands/music/play.js
@@ -49,6 +49,174 @@ module.exports = {
         let videoUrl = "https://www.youtube.com/watch?v=" + query[0].videoId;
         addQueue(videoUrl);
       }
+
+      //functions
+      async function addQueue(url) {
+        let songInfo = await ytcore.getInfo(url);
+        let song = {
+          title: songInfo.title,
+          url: songInfo.video_url,
+          thumbnail: getThumbnail(url),
+          duration: secondsCoverter(songInfo.length_seconds),
+          seconds: songInfo.length_seconds,
+          requester: message.author.tag,
+        };
+        if (!serverQueue) {
+          let tempQueue = {
+            guildID: null,
+            queue: [],
+            isPlaying: false,
+            voiceChannel: voiceChannel,
+            textChannel: message.channel,
+            connection: null,
+            dispatcher: null,
+          };
+          tempQueue.queue.push(song);
+          tempQueue.connection = await tempQueue.voiceChannel.join();
+          client.queue.set(message.guild.id, tempQueue);
+          play(message.guild.id);
+        } else {
+          serverQueue.queue.push(song);
+          message.channel.send({
+            embed: {
+              color: 3066993,
+              title: "Queue added",
+              url: serverQueue.queue[serverQueue.queue.length - 1].url,
+              description:
+                serverQueue.queue[serverQueue.queue.length - 1].title,
+              thumbnail: {
+                url: serverQueue.queue[serverQueue.queue.length - 1].thumbnail,
+              },
+              footer: {
+                text:
+                  `Duration ` +
+                  serverQueue.queue[serverQueue.queue.length - 1].duration,
+              },
+            },
+          });
+        }
+      }
+      async function play(guild) {
+        const serverQueue = client.queue.get(guild);
+        if (!serverQueue.queue[0]) {
+          serverQueue.isPlaying = false;
+          updatePresence(serverQueue);
+          serverQueue.voiceChannel.leave();
+          message.channel.send({
+            embed: {
+              color: 15158332,
+              title: "No songs in the queue",
+              author: {
+                name: message.client.user.username,
+                icon_url: message.client.user.avatarURL({
+                  format: "png",
+                  dynamic: true,
+                  size: 1024,
+                }),
+              },
+            },
+          });
+          client.queue.delete(guild);
+        } else {
+          message.channel.send({
+            embed: {
+              color: 3447003,
+              title: "Playing",
+              url: serverQueue.queue[0].url,
+              description: serverQueue.queue[0].title,
+              thumbnail: {
+                url: serverQueue.queue[0].thumbnail,
+              },
+              footer: {
+                text: `Duration ` + serverQueue.queue[0].duration,
+              },
+            },
+          });
+          serverQueue.dispatcher = serverQueue.connection
+            .play(
+              ytcore(serverQueue.queue[0].url, {
+                filter: "audioonly",
+                quality: "highestaudio",
+                highWaterMark: 1 << 25,
+                encoderArgs: [
+                  "-af",
+                  `equalizer=f=40:width_type=h:width=50:g=50`,
+                ],
+              })
+            )
+            // serverQueue.dispatcher = serverQueue.connection
+            //   .play(await ytDiscord(serverQueue.queue[0].url), { type: "opus" })
+            .on("start", () => {
+              serverQueue.isPlaying = true;
+              updatePresence(serverQueue);
+              addTopSong(serverQueue.queue[0].title);
+            })
+            .on("finish", () => {
+              console.log("stop playing");
+              serverQueue.queue.shift();
+              play(message.guild.id);
+            })
+            .on("volumeChange", (oldVolume, newVolume) => {
+              message.channel.send({
+                embed: {
+                  title: `Volume changed from ${oldVolume} to ${newVolume}.`,
+                  author: {
+                    name: message.client.user.username,
+                    icon_url: message.client.user.avatarURL({
+                      format: "png",
+                      dynamic: true,
+                      size: 1024,
+                    }),
+                  },
+                },
+              });
+            })
+            .on("end", () => {
+              serverQueue.queue.shift();
+            })
+            .on("error", (error) => {
+              console.log(error);
+            });
+        }
+      }
+
+      function getThumbnail(url) {
+        let ids = getVideoId(url);
+        return `http://img.youtube.com/vi/${ids.id}/maxresdefault.jpg`;
+      }
+      function addTopSong(title) {
+        musicDB.updateCount(title);
+      }
+      function secondsCoverter(second) {
+        var timestamp = second;
+
+        // 2
+        var hours = Math.floor(timestamp / 60 / 60);
+
+        // 37
+        var minutes = Math.floor(timestamp / 60) - hours * 60;
+
+        // 42
+        var seconds = timestamp % 60;
+        if (hours > 0) {
+          return hours + ":" + minutes + ":" + seconds;
+        } else return minutes + ":" + seconds;
+      }
+      function updatePresence(serverQueue) {
+        if (serverQueue.isPlaying === true) {
+          message.member.guild.channels.cache
+            .find((x) => x.id === serverQueue.textChannel.id)
+            .setTopic("Playing " + serverQueue.queue[0].title);
+        }
+        if (serverQueue.isPlaying === false) {
+          message.member.guild.channels.cache
+            .find((x) => x.id === serverQueue.textChannel.id)
+            .setTopic("Not playing");
+        }
+        if (message.content.includes("--lyrics")) {
+          // util;
+        }
+      }
     } catch (error) {
       message.channel.send({
         embed: {
@@ -57,169 +225,6 @@ module.exports = {
           description: error.message,
         },
       });
-    }
-    //functions
-    async function addQueue(url) {
-      let songInfo = await ytcore.getInfo(url);
-      let song = {
-        title: songInfo.title,
-        url: songInfo.video_url,
-        thumbnail: getThumbnail(url),
-        duration: secondsCoverter(songInfo.length_seconds),
-        seconds: songInfo.length_seconds,
-        requester: message.author.tag,
-      };
-      if (!serverQueue) {
-        let tempQueue = {
-          guildID: null,
-          queue: [],
-          isPlaying: false,
-          voiceChannel: voiceChannel,
-          textChannel: message.channel,
-          connection: null,
-          dispatcher: null,
-        };
-        tempQueue.queue.push(song);
-        tempQueue.connection = await tempQueue.voiceChannel.join();
-        client.queue.set(message.guild.id, tempQueue);
-        play(message.guild.id);
-      } else {
-        serverQueue.queue.push(song);
-        message.channel.send({
-          embed: {
-            color: 3066993,
-            title: "Queue added",
-            url: serverQueue.queue[serverQueue.queue.length - 1].url,
-            description: serverQueue.queue[serverQueue.queue.length - 1].title,
-            thumbnail: {
-              url: serverQueue.queue[serverQueue.queue.length - 1].thumbnail,
-            },
-            footer: {
-              text:
-                `Duration ` +
-                serverQueue.queue[serverQueue.queue.length - 1].duration,
-            },
-          },
-        });
-      }
-    }
-    async function play(guild) {
-      const serverQueue = client.queue.get(guild);
-      if (!serverQueue.queue[0]) {
-        serverQueue.isPlaying = false;
-        updatePresence(serverQueue);
-        serverQueue.voiceChannel.leave();
-        message.channel.send({
-          embed: {
-            color: 15158332,
-            title: "No songs in the queue",
-            author: {
-              name: message.client.user.username,
-              icon_url: message.client.user.avatarURL({
-                format: "png",
-                dynamic: true,
-                size: 1024,
-              }),
-            },
-          },
-        });
-        client.queue.delete(guild);
-      } else {
-        message.channel.send({
-          embed: {
-            color: 3447003,
-            title: "Playing",
-            url: serverQueue.queue[0].url,
-            description: serverQueue.queue[0].title,
-            thumbnail: {
-              url: serverQueue.queue[0].thumbnail,
-            },
-            footer: {
-              text: `Duration ` + serverQueue.queue[0].duration,
-            },
-          },
-        });
-        serverQueue.dispatcher = serverQueue.connection
-          .play(
-            ytcore(serverQueue.queue[0].url, {
-              filter: "audioonly",
-              quality: "highestaudio",
-              highWaterMark: 1 << 25,
-              encoderArgs: ["-af", `equalizer=f=40:width_type=h:width=50:g=50`],
-            })
-          )
-          // serverQueue.dispatcher = serverQueue.connection
-          //   .play(await ytDiscord(serverQueue.queue[0].url), { type: "opus" })
-          .on("start", () => {
-            serverQueue.isPlaying = true;
-            updatePresence(serverQueue);
-            addTopSong(serverQueue.queue[0].title);
-          })
-          .on("finish", () => {
-            console.log("stop playing");
-            serverQueue.queue.shift();
-            play(message.guild.id);
-          })
-          .on("volumeChange", (oldVolume, newVolume) => {
-            message.channel.send({
-              embed: {
-                title: `Volume changed from ${oldVolume} to ${newVolume}.`,
-                author: {
-                  name: message.client.user.username,
-                  icon_url: message.client.user.avatarURL({
-                    format: "png",
-                    dynamic: true,
-                    size: 1024,
-                  }),
-                },
-              },
-            });
-          })
-          .on("end", () => {
-            serverQueue.queue.shift();
-          })
-          .on("error", (error) => {
-            console.log(error);
-          });
-      }
-    }
-
-    function getThumbnail(url) {
-      let ids = getVideoId(url);
-      return `http://img.youtube.com/vi/${ids.id}/maxresdefault.jpg`;
-    }
-    function addTopSong(title) {
-      musicDB.updateCount(title);
-    }
-    function secondsCoverter(second) {
-      var timestamp = second;
-
-      // 2
-      var hours = Math.floor(timestamp / 60 / 60);
-
-      // 37
-      var minutes = Math.floor(timestamp / 60) - hours * 60;
-
-      // 42
-      var seconds = timestamp % 60;
-      if (hours > 0) {
-        return hours + ":" + minutes + ":" + seconds;
-      } else return minutes + ":" + seconds;
-    }
-    function updatePresence(serverQueue) {
-      if (serverQueue.isPlaying === true) {
-        message.member.guild.channels.cache
-          .find((x) => x.id === serverQueue.textChannel.id)
-          .setTopic("Playing " + serverQueue.queue[0].title);
-      }
-      if (serverQueue.isPlaying === false) {
-        message.member.guild.channels.cache
-          .find((x) => x.id === serverQueue.textChannel.id)
-          .setTopic("Not playing");
-      }
-      if (message.content.includes("--lyrics")) {
-        // util;
-      }
     }
   },
 };
