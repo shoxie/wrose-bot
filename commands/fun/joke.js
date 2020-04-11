760ca85e2e1d0cbd33876355893df7ec94a54554
wrose <sktt1lka@gmail.com>
2020-04-11 11:23:13 +0700

    Create joke.js

---
+++ commands/fun/joke.js
@@ -0,0 +1,17 @@
+const DadJokes = require("dadjokes-wrapper");
+const joke = new DadJokes();
+
+module.exports = {
+config : {
+name :"joke",
+usage:"joke",
+description:"Get random joke",
+aliases:[],
+enabled: true, 
+ownerOnly: false
+},
+async run(client, message, args) {
+const dadjoke = await joke.randomJoke();
+    message.reply(dadjoke);
+}
+}
