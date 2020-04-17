1e4198c52c80ddb4c9761c777005059c81e34209
shoxie <sktt1lka@gmail.com>
2020-04-17 19:14:58 +0700

    fix disconnect command

--- commands/music/disconnect.js
+++ commands/music/disconnect.js
@@ -1,24 +1,24 @@
-let serverQueue = require("../../model/model.js");
+let { stop } = require("../../utils/radio");
 module.exports = {
   config: {
     name: "disconnect",
     usage: "disconnect",
     description: "Disconnect me from the voiceChannel",
     ownerOnly: false,
-    enabled: true
+    enabled: true,
   },
   async run(client, message, args) {
     let serverQueue = client.queue.get(message.guild.id);
-    let normaldj = message.guild.roles.cache.find(x => x.name === "dj");
-    let bigdj = message.guild.roles.cache.find(x => x.name === "DJ");
+    let normaldj = message.guild.roles.cache.find((x) => x.name === "dj");
+    let bigdj = message.guild.roles.cache.find((x) => x.name === "DJ");
     let roleID = bigdj ? bigdj : normaldj;
     if (!roleID) {
       return message.channel.send({
         embed: {
           color: 15158332,
           title:
-            "Your guild does not have the DJ role, please contact your guild owner/administrator/moderator to create and add the role."
-        }
+            "Your guild does not have the DJ role, please contact your guild owner/administrator/moderator to create and add the role.",
+        },
       });
     }
     if (!serverQueue.voiceChannel) {
@@ -32,30 +32,32 @@ module.exports = {
             icon_url: message.client.user.avatarURL({
               format: "png",
               dynamic: true,
-              size: 1024
-            })
-          }
-        }
+              size: 1024,
+            }),
+          },
+        },
       });
     }
     if (serverQueue.voiceChannel) {
       if (message.member.roles.cache.has(roleID.id)) {
-        serverQueuel.queue = [];
-        serverQueue.connection.dispatcher.end();
-        message.channel.send({
-          embed: {
-            title: "Disconnected from voiceChannel",
-            author: {
-              name: message.client.user.username,
-              icon_url: message.client.user.avatarURL({
-                format: "png",
-                dynamic: true,
-                size: 1024
-              })
-            }
-          }
-        });
+        if (!serverQueue.radio) {
+          serverQueue.queue = [];
+          serverQueue.connection.dispatcher.end();
+          message.channel.send({
+            embed: {
+              title: "Disconnected from voiceChannel",
+              author: {
+                name: message.client.user.username,
+                icon_url: message.client.user.avatarURL({
+                  format: "png",
+                  dynamic: true,
+                  size: 1024,
+                }),
+              },
+            },
+          });
+        } else stop(client, message);
       } else return message.channel.send("Mày định đảo chính hả ???");
     }
-  }
+  },
 };
