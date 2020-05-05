cdea9296851c0cb5ea17e4ddd1f757a1bfb10758
shoxie <sktt1lka@gmail.com>
2020-05-05 23:07:14 +0700

    word chain fix again

--- commands/game/wordChain.js
+++ commands/game/wordChain.js
@@ -4,6 +4,7 @@ const util = require("../../utils/utility");
 const { redMessage } = require("../../utils/message");
 var checkWord = require("check-word");
 var check = checkWord("en");
+var request = require("request");
 module.exports = {
   config: {
     name: "wordChain",
@@ -13,55 +14,55 @@ module.exports = {
     ownerOnly: false,
     enabled: true,
   },
-  async run(client, message, args) {
+  async run(client, msg, args) {
+    let message = msg;
     let time = 10;
     const opponent = message.mentions.users.first();
-    if (opponent.bot) return message.reply("Bots may not be played against.");
-    if (opponent.id === message.author.id)
-      return message.reply("You may not play against yourself.");
-    const current = client.games.get(message.channel.id);
+
+    if (opponent.bot) return msg.reply("Bots may not be played against.");
+    if (opponent.id === msg.author.id)
+      return msg.reply("You may not play against yourself.");
+    const current = this.client.games.get(msg.channel.id);
     if (current)
-      return message.reply(
+      return msg.reply(
         `Please wait until the current game of \`${current.name}\` is finished.`
       );
-    client.games.set(message.channel.id, { name: this.config.name });
+    this.client.games.set(msg.channel.id, { name: this.name });
     try {
-      await message.channel.send(`${opponent}, do you accept this challenge?`);
-      const verification = await util.verify(message.channel, opponent);
+      await msg.say(`${opponent}, do you accept this challenge?`);
+      const verification = await util.verify(msg.channel, opponent);
       if (!verification) {
-        client.games.delete(message.channel.id);
-        return message.channel.send("Looks like they declined...");
+        this.client.games.delete(msg.channel.id);
+        return msg.say("Looks like they declined...");
       }
       const startWord =
         startWords[Math.floor(Math.random() * startWords.length)];
-      await message.channel.send(stripIndents`
+      await msg.say(stripIndents`
 				The start word will be **${startWord}**! You must answer within **${time}** seconds!
 				If you think your opponent has played a word that doesn't exist, respond with **challenge** on your turn.
 				Words cannot contain anything but letters. No numbers, spaces, or hyphens may be used.
 				The game will start in 5 seconds...
 			`);
-      await util.delay(5000);
+      await delay(5000);
       let userTurn = Boolean(Math.floor(Math.random() * 2));
       const words = [];
       let winner = null;
       let lastWord = startWord;
       while (!winner) {
-        const player = userTurn ? message.author : opponent;
+        const player = userTurn ? msg.author : opponent;
         const letter = lastWord.charAt(lastWord.length - 1);
-        await message.channel.send(
-          `It's ${player}'s turn! The letter is **${letter}**.`
-        );
+        await msg.say(`It's ${player}'s turn! The letter is **${letter}**.`);
         const filter = (res) =>
           res.author.id === player.id &&
           /^[a-zA-Z']+$/i.test(res.content) &&
           res.content.length < 50;
-        const wordChoice = await message.channel.awaitMessages(filter, {
+        const wordChoice = await msg.channel.awaitMessages(filter, {
           max: 1,
           time: time * 1000,
         });
         if (!wordChoice.size) {
-          await message.channel.send("Time!");
-          winner = userTurn ? opponent : message.author;
+          await msg.say("Time!");
+          winner = userTurn ? opponent : msg.author;
           break;
         }
         const choice = wordChoice.first().content.toLowerCase();
@@ -75,28 +76,36 @@ module.exports = {
           await msg.say(`Sorry, **${lastWord}** is indeed valid!`);
           continue;
         }
-        if (!choice.startsWith(letter) || words.includes(choice) || !check.check(choice)) {
-          await message.channel.send("Sorry! You lose!");
-          winner = userTurn ? opponent : message.author;
+        if (!choice.startsWith(letter) || words.includes(choice)) {
+          await msg.say("Sorry! You lose!");
+          winner = userTurn ? opponent : msg.author;
           break;
         }
         words.push(choice);
         lastWord = choice;
         userTurn = !userTurn;
       }
-      client.games.delete(message.channel.id);
-      if (!winner) return message.channel.send("Oh... No one won.");
-      let a = "";
-      for (let word of words) {
-        a = a + word + ", ";
-      }
-      redMessage(message, "Game result", a);
-      return message.channel.send(
-        `The game is over! The winner is ${winner}! \n ${words.length} was answered`
-      );
+      this.client.games.delete(msg.channel.id);
+      if (!winner) return msg.say("Oh... No one won.");
+      return msg.say(`The game is over! The winner is ${winner}!`);
     } catch (err) {
-      client.games.delete(message.channel.id);
+      this.client.games.delete(msg.channel.id);
       throw err;
+    }
+    async function verifyWord(word) {
+      if (startWords.includes(word.toLowerCase())) return true;
+      try {
+        const { body } = await request
+          .get(
+            `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}`
+          )
+          .query({ key: "032fbf78-d658-49c9-afc7-4c6f1d420d15" });
+        if (!body.length) return false;
+        return true;
+      } catch (err) {
+        if (err.status === 404) return false;
+        return null;
+      }
     }
   },
 };
