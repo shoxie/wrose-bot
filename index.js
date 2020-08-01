f60f57ac4d665eb8c0eb4e44cb03bbc57cdd3ec5
shoxie <sktt1lka@gmail.com>
2020-08-02 04:52:26 +0700

    auto skip a song when downvoted

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const Discord = require('discord.js')
+const Discord = require("discord.js");
 const client = new Discord.Client({
   disableEveryone: true,
-  fetchAllMembers: true
-})
-const config = require('./config/config.json')
-const mongoose = require('mongoose')
-const logs = require('discord-logs')
-logs(client)
-process.env.NODE_PATH = __dirname
-require('dotenv').config()
-const eventHandler = require('./handler/eventHandler.js')(client)
-const commandHandler = require('./handler/commandHandler.js')(client)
-client.login(process.env.token)
+  fetchAllMembers: true,
+});
+const config = require("./config/config.json");
+const mongoose = require("mongoose");
+const logs = require("discord-logs");
+logs(client);
+process.env.NODE_PATH = __dirname;
+require("dotenv").config();
+const eventHandler = require("./handler/eventHandler.js")(client);
+const commandHandler = require("./handler/commandHandler.js")(client);
+client.login(process.env.token);
 
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
-    useCreateIndex: true
+    useCreateIndex: true,
   })
   .then(() => {
-    console.log('CONNECTED DIT CON ME MAY')
-  })
+    console.log("CONNECTED DIT CON ME MAY");
+  });
--- utils/message.js
+++ utils/message.js
@@ -1,55 +1,73 @@
 const sendSongQueue = (message, client) => {
-  const serverQueue = client.queue.get(message.guild.id)
+  const serverQueue = client.queue.get(message.guild.id);
   message.channel.send({
     embed: {
       color: 3066993,
-      title: 'Queue added',
+      title: "Queue added",
       url: serverQueue.queue[serverQueue.queue.length - 1].url,
       description: serverQueue.queue[serverQueue.queue.length - 1].title,
       thumbnail: {
-        url: serverQueue.queue[serverQueue.queue.length - 1].thumbnail
+        url: serverQueue.queue[serverQueue.queue.length - 1].thumbnail,
       },
       footer: {
         text:
-          'Duration ' +
-          serverQueue.queue[serverQueue.queue.length - 1].duration
-      }
-    }
-  })
-}
-const sendPlaying = (message, client) => {
-  const serverQueue = client.queue.get(message.guild.id)
-  message.channel.send({
+          "Duration " +
+          serverQueue.queue[serverQueue.queue.length - 1].duration,
+      },
+    },
+  });
+};
+const sendPlaying = async (message, client) => {
+  const serverQueue = client.queue.get(message.guild.id);
+  const queueMsg = await message.channel.send({
     embed: {
       color: 3447003,
-      title: 'Playing',
+      title: "Playing",
       url: serverQueue.queue[0].url,
       description: serverQueue.queue[0].title,
       thumbnail: {
-        url: serverQueue.queue[0].thumbnail
+        url: serverQueue.queue[0].thumbnail,
       },
       footer: {
-        text: 'Duration ' + serverQueue.queue[0].duration
-      }
+        text: "Duration " + serverQueue.queue[0].duration,
+      },
+    },
+  });
+  await queueMsg.react("👍");
+  await queueMsg.react("👎");
+  var dislike = 0;
+  const filter = (reaction, user) =>
+    reaction.emoji.name === "👎" && user.id !== client.user.id;
+  const collector = queueMsg.createReactionCollector(filter, {
+    time: serverQueue.queue[0].seconds * 1000,
+  });
+  collector.on("collect", (r) => {
+    dislike++;
+    if (dislike > message.member.voice.channel.members.filter(user => !user.user.bot).size / 2) {
+      serverQueue.connection.dispatcher.end();
+      collector.stop();
     }
-  })
-}
+  });
+  collector.on("end", (collected) => {
+    if (queueMsg) queueMsg.delete();
+  });
+};
 const emptyQueue = (message) => {
   message.channel.send({
     embed: {
       color: 15158332,
-      title: 'No songs in the queue',
+      title: "No songs in the queue",
       author: {
         name: message.client.user.username,
         icon_url: message.client.user.avatarURL({
-          format: 'png',
+          format: "png",
           dynamic: true,
-          size: 1024
-        })
-      }
-    }
-  })
-}
+          size: 1024,
+        }),
+      },
+    },
+  });
+};
 const redMessage = (message, title, description = null) => {
   message.channel.send({
     embed: {
@@ -59,14 +77,14 @@ const redMessage = (message, title, description = null) => {
       author: {
         name: message.client.user.username,
         icon_url: message.client.user.avatarURL({
-          format: 'png',
+          format: "png",
           dynamic: true,
-          size: 1024
-        })
-      }
-    }
-  })
-}
+          size: 1024,
+        }),
+      },
+    },
+  });
+};
 const blueMessage = (message, title, description = null) => {
   message.channel.send({
     embed: {
@@ -76,37 +94,37 @@ const blueMessage = (message, title, description = null) => {
       author: {
         name: message.client.user.username,
         icon_url: message.client.user.avatarURL({
-          format: 'png',
+          format: "png",
           dynamic: true,
-          size: 1024
-        })
-      }
-    }
-  })
-}
+          size: 1024,
+        }),
+      },
+    },
+  });
+};
 const sendLyrics = async (message, lyrics) => {
-  var output = lyrics.split('\n')
-  var myfields = []
-  var tmp = 0
-  var sttmp = ''
+  var output = lyrics.split("\n");
+  var myfields = [];
+  var tmp = 0;
+  var sttmp = "";
   for (var i = 0; i <= output.length; i++) {
-    sttmp += output[i] + ' \n '
-    tmp++
+    sttmp += output[i] + " \n ";
+    tmp++;
     if (tmp == 15) {
-      myfields.push({ name: '\u200B', value: sttmp })
-      tmp = 0
-      sttmp = ''
+      myfields.push({ name: "\u200B", value: sttmp });
+      tmp = 0;
+      sttmp = "";
     }
   }
-}
+};
 const tryAgain = async (message) => {
-  message.reply('Something happened. Please try again later')
-}
+  message.reply("Something happened. Please try again later");
+};
 module.exports = {
   sendSongQueue,
   sendPlaying,
   emptyQueue,
   blueMessage,
   redMessage,
-  tryAgain
-}
+  tryAgain,
+};
